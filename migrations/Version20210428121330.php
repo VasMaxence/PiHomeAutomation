187a8df94f7325136edea3468ab5178467f0e8a1
maxence <maxence.vasseur@epitech.eu>
2021-05-02 15:16:22 +0200

    ---- SP/10-Login-Page ----
    [*] .env: patch url database.
    [+/*] Bootstrap.*: Adding, update css for row and width.
    [+] jquery.min.js: Adding jquery dependencies to the project.
    [+] commun.twig: Create main view, all view have to extends form her.
    [+] config.scss: create config file for scss, in this file we have to set all scss variable.
    [*] DashboardController.php: update with redirect url if user was no connected.
    [*] login.twig: create view for login page.
    [+] LoginFormAuthenticator.php: Create class to handle login event with symfony.
    [+] LogoutController.php: handle logout event and route.
    [+] messages.*.xliff: create translating config files.
    [+] User.php / UserRepository.php: create User Database.
    [+] Version*.php: create file to update your Database.

---
+++ migrations/Version20210428121330.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210428121330 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(32) NOT NULL, firstname VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, is_active TINYINT(1) NOT NULL, created_date DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), UNIQUE INDEX UNIQ_1483A5E9F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('DROP TABLE test');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE test (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE users');
+    }
+}
--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,16 +3,21 @@
 namespace App\Controller;
 
 use App\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
- * @Route("/dashboard", name="dashboard_controller", methods={"GET"})
+ * @Route("/dashboard", name="dashboard_controller::", methods={"GET"})
  */
 class DashboardController extends Controller
 {
-    public function __construct() {
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator) {
+        $this->urlGenerator = $urlGenerator;
         parent::__construct("Dashboard", ["dylab" => "bo"]);
     }
 
@@ -23,6 +28,8 @@ class DashboardController extends Controller
      */
     public function home(Request $request): Response
     {
+        if (!$this->getUser())
+            return new RedirectResponse($this->urlGenerator->generate('security_controller::security_login'));
         $this->initController();
         $this->addRender('dashboard/home.twig', array(
             "controller_name" => "DashboardController::Home"
